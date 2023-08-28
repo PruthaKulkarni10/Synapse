f11bbf4f61b47e90e3e6316d1551c375920e77e8
PruthaKulkarni10 <130536825+PruthaKulkarni10@users.noreply.github.com>
2023-08-28 17:25:52 +0530

    task_1.2.py

--- Synapse Task 1/task_1.2.py
+++ Synapse Task 1/task_1.2.py
@@ -1,24 +1,40 @@
-lst=['0001','0011','0101','1011']
+lst=['0001','0011','0101','1011','1101','1111']
 new_list=[]
 
 for i in lst:
     x=int(i,2)
     new_list.append(x)
 
-
-for i in range(len(new_list)-3):
+print(new_list)
+for i in range(len(new_list)-5):
      
-      sum=new_list[i+1]+new_list[i+2]
+      sum=new_list[i]+new_list[i+5]
       
       new_list.append(sum)
-      new_list.pop(i+1)
-      new_list.pop(i+1)
+      new_list.pop(i)
+      new_list.pop(i+4)
+print(new_list)
     
-for i in range(len(new_list)-2):
-     sum=new_list[i]+new_list[i+2]
+for i in range(len(new_list)-4):
+     sum=new_list[i]+new_list[i+1]
      new_list.append(sum)
      new_list.pop(i)
-     new_list.pop(i+1)
+     new_list.pop(i)
+    
+print(new_list)
 
+for i in range (len(new_list)-3):
+     sum=new_list[i]+ new_list[i+1]
+     new_list.append(sum)
+     new_list.pop(i)
+     new_list.pop(i)
+
+print(new_list)
+
+for i in range (len(new_list)-2):
+     sum=new_list[i]+ new_list[i+1]
+     new_list.append(sum)
+     new_list.pop(i)
+     new_list.pop(i)
 
 print(new_list)
